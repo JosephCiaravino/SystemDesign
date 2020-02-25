f386a3febfd9331bb5b4dc72b684a30c417a15df
Joseph Ciaravino <josephciaravino@Josephs-MacBook-Pro-2.local>
2020-02-25 11:49:42 -0500

    put redirects in user proccess page

--- SysDev/private/process_user.php
+++ SysDev/private/process_user.php
@@ -1,14 +1,25 @@
 <?php 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
   if (empty($_POST["loginIdentity"]) || empty($_POST["password"])) {
   	header("Location: ../public/login.php");
-  }else {
-   	var_dump($_POST);
+
+  }else if($_POST["loginIdentity"] == 'john@weu.edu' && $_POST["password"] == 'stuPass'){
+   	header("Location: ../private/userAreas/student/index.php");
     
+  }else if($_POST["loginIdentity"] == 'jane@weu.edu' && $_POST["password"] == 'facPass'){
+   	header("Location: ../private/userAreas/faculty/index.php");
+
+  }else if($_POST["loginIdentity"] == 'jamie@weu.edu' && $_POST["password"]== 'adminPass'){
+  	header("Location: ../private/userAreas/admin/index.php");
+
+  }else if($_POST["loginIdentity"] == 'jill@weu.edu' && $_POST["password"]== 'resPass'){
+  	header("Location: ../private/userAreas/researcher/index.php");
+
+  }else{
+  	var_dump($_POST);
   }
 
-}
+
 		
 
 ?>
